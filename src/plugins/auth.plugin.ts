bca63e15353ced74976662d10e42fa23ee47aba6
poonsht <poon_arsene_lupin@hotmail.com>
2020-12-28 16:51:25 +0700

    [+] fix : indent

--- src/plugins/auth.plugin.ts
+++ src/plugins/auth.plugin.ts
@@ -1,27 +1,39 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 import fastifyJwt from 'fastify-jwt'
+// import fastifyCookie from 'fastify-cookie'
 import config from '../config/config'
 import { responseSender } from '../helper/response.handler'
 import parseResponse from '../helper/response.parser'
+// import accountAdapter from '../adapters/account.adapter'
 
 const authPlugin = (fastify: FastifyInstance, opts: FastifyPluginOptions, done: any) => {
+  fastify.register(fastifyJwt, {
+    secret: config.jwt.private_route.secret.jwt_secret,
+  })
 
-    fastify.register(fastifyJwt, {
-        secret: config.jwt.private_route.secret.jwt_secret
-    })
+  fastify.decorate('verifyAuth', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      //   const auth: string | undefined = request.headers.authorization
+      //   const token = auth!.split(' ')[1]
+      //   if (token) {
+      //     const decodedToken: any = fastify.jwt.decode(token)
+      //     const { exp } = decodedToken
+      //     if (exp < new Date().getTime() / 1000) {
+      //       const { refresh_token } = request.cookies
+      //       const res = (await accountAdapter.generateAccessTokenFromRefreshToken(refresh_token)) as any
 
-    fastify.decorate("verifyAuth", async (request: FastifyRequest, reply: FastifyReply) => {
-        try {
-            await request.jwtVerify()
-        } catch (err) {
-            responseSender(
-                parseResponse(new Error(`${err.statusCode}: Unauthorize, ${err.message}`))
-            , reply)
-        }
-    })
+      //       const { access_token } = res.data
+      //       request.headers.authorization = `Bearer ${access_token}`
+      //     }
+      //   }
+      await request.jwtVerify()
+    } catch (err) {
+      responseSender(parseResponse(new Error(`${err.statusCode}: Unauthorize, ${err.message}`)), reply)
+    }
+  })
 
-    done()
+  done()
 }
 
 export default fastifyPlugin(authPlugin)
--- src/routes/shipper.route.ts
+++ src/routes/shipper.route.ts
@@ -2,33 +2,43 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 import ShipperUsecase from '../usecase/shipper.usecase'
 import responseHandler from '../helper/response.handler'
 import { Payload } from '../entities/dtos/token.dto'
-import { createDTO, confirmedEmailDTO,  deleteDTO, identifierDTO, updateJobHistoryDTO , whitelistUpdateProfileDTO} from '../entities/dtos/shipper.dto'
+import {
+  createDTO,
+  confirmedEmailDTO,
+  deleteDTO,
+  identifierDTO,
+  updateJobHistoryDTO,
+  whitelistUpdateProfileDTO,
+} from '../entities/dtos/shipper.dto'
 import * as Validator from '../helper/validate.helper'
 
 class ShipperRoutes {
   public prefix_route = '/shipper'
 
   async routes(fastify: FastifyInstance, opts: FastifyPluginOptions, done: any) {
-
     fastify.get(`/healthcheck`, async (request, reply) => {
       responseHandler(async () => {
-        return {healthcheck: "server is alive"}
+        return { healthcheck: 'server is alive' }
       }, reply)
       await reply
     })
 
-    fastify.get(`/admin/profile/:username`, { preValidation: [(fastify as any).verifyAuth] }, async (request, reply) => {
-      responseHandler(async () => {
-        const param: identifierDTO = request.params as identifierDTO   
-        const data = await ShipperUsecase.adminFindShipperByIdentifier(param)
-        return data
-      }, reply)
-      await reply
-    })
+    fastify.get(
+      `/admin/profile/:username`,
+      { preValidation: [(fastify as any).verifyAuth] },
+      async (request, reply) => {
+        responseHandler(async () => {
+          const param: identifierDTO = request.params as identifierDTO
+          const data = await ShipperUsecase.adminFindShipperByIdentifier(param)
+          return data
+        }, reply)
+        await reply
+      },
+    )
 
     fastify.get(`/profile/:username`, async (request, reply) => {
       responseHandler(async () => {
-        const param: identifierDTO = request.params as identifierDTO   
+        const param: identifierDTO = request.params as identifierDTO
         const data = await ShipperUsecase.findProfileShipperAccountByUsername(param)
         return data
       }, reply)
@@ -37,31 +47,28 @@ class ShipperRoutes {
 
     fastify.post(`/create`, async (request, reply) => {
       responseHandler(async () => {
-        const req: createDTO = request.body as createDTO   
-        let { email, ...shipper_account } = req 
+        const req: createDTO = request.body as createDTO
+        let { email, ...shipper_account } = req
         const data = await ShipperUsecase.createShipperAccount(shipper_account)
         return data
-
       }, reply)
       await reply
     })
-    
+
     // This route have vulnerability at client, we should use this route service to service for policy.
     fastify.put(`/confirmed_email`, { preValidation: [(fastify as any).verifyAuth] }, async (request, reply) => {
       responseHandler(async () => {
         const req: confirmedEmailDTO = request.body as confirmedEmailDTO
-        let { email, identifier } = req 
-        
-        if(!email)
-          throw new Error(`400 : Invalid input, Please input field email`)  
+        let { email, identifier } = req
 
-        if('username' in identifier || 'shipper_id' in identifier){
+        if (!email) throw new Error(`400 : Invalid input, Please input field email`)
+
+        if ('username' in identifier || 'shipper_id' in identifier) {
           const data = await ShipperUsecase.confirmedWithEmail(req)
-          return data  
+          return data
         } else {
-          throw new Error(`400 : Invalid input, Please input field username or account id`)  
+          throw new Error(`400 : Invalid input, Please input field username or account id`)
         }
-        
       }, reply)
       await reply
     })
@@ -70,15 +77,13 @@ class ShipperRoutes {
       responseHandler(async () => {
         const req: deleteDTO = request.body as deleteDTO
         const { username } = request.user as Payload
-        const identifier: identifierDTO  = { username }
+        const identifier: identifierDTO = { username }
         let data
 
-        if(identifier.username || identifier.shipper_id)
-          data = await ShipperUsecase.deleteShipperAccount({ ...req, identifier})
-        else 
-          throw new Error(`400 : Invalid input, Input not exist account id or password field`)  
+        if (identifier.username || identifier.shipper_id)
+          data = await ShipperUsecase.deleteShipperAccount({ ...req, identifier })
+        else throw new Error(`400 : Invalid input, Input not exist account id or password field`)
         return data
-
       }, reply)
       await reply
     })
@@ -87,13 +92,12 @@ class ShipperRoutes {
       responseHandler(async () => {
         const req: deleteDTO = request.body as deleteDTO
         const { username } = request.user as Payload
-        const identifier: identifierDTO  = { username }
+        const identifier: identifierDTO = { username }
         let data
 
-        if(identifier.username || identifier.shipper_id)
+        if (identifier.username || identifier.shipper_id)
           data = await ShipperUsecase.deActivateShipperAccount({ ...req, identifier })
-        else 
-          throw new Error(`400 : Invalid input, Input not exist account id or password field`) 
+        else throw new Error(`400 : Invalid input, Input not exist account id or password field`)
         return data
       }, reply)
       await reply
@@ -102,15 +106,14 @@ class ShipperRoutes {
     fastify.put(`/profile/update`, { preValidation: [(fastify as any).verifyAuth] }, async (request, reply) => {
       responseHandler(async () => {
         const { username } = request.user as Payload
-        const identifier: identifierDTO  = { username }
+        const identifier: identifierDTO = { username }
         const profile: whitelistUpdateProfileDTO = request.body as whitelistUpdateProfileDTO
 
-        if(identifier.username || identifier.shipper_id){
+        if (identifier.username || identifier.shipper_id) {
           const errorFieldsUpdate = Validator.validUpdatedFields(profile, 'shipper')
-          if (errorFieldsUpdate.length > 0) 
-            throw new Error(`400 : Invalid Fields! ${errorFieldsUpdate.join(', ')}`)
-        } else{
-          throw new Error(`400 : Invalid input, Input not exist account id or password field`) 
+          if (errorFieldsUpdate.length > 0) throw new Error(`400 : Invalid Fields! ${errorFieldsUpdate.join(', ')}`)
+        } else {
+          throw new Error(`400 : Invalid input, Input not exist account id or password field`)
         }
 
         const data = await ShipperUsecase.updateProfileShipperAccount({ identifier, profile })
@@ -129,9 +132,7 @@ class ShipperRoutes {
     })
 
     done()
-
   }
-
 }
 
 export default ShipperRoutes
