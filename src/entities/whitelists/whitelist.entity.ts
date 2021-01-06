eddf0c552c7f0b15d73e08bdd26dcc1fec0707ba
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-06 15:49:51 +0700

    [.] indent change

--- src/entities/whitelists/whitelist.entity.ts
+++ src/entities/whitelists/whitelist.entity.ts
@@ -2,11 +2,11 @@ import { Whitelists } from '../interfaces/data/whitelist.interface'
 
 export const whitelist: Whitelists = {
   shipper: {
-    name: {field: 'name'},
-    display_name: {field: 'display_name'},
-    tel: {field: 'tel'},
-    address: {field: 'address'},
-    account_description: {field: 'account_description'},
-    juristic_id: {field: 'juristic_id'},
-  }
+    name: { field: 'name' },
+    display_name: { field: 'display_name' },
+    tel: { field: 'tel' },
+    address: { field: 'address' },
+    account_description: { field: 'account_description' },
+    juristic_id: { field: 'juristic_id' },
+  },
 }
--- src/repositories/account.repository.ts
+++ src/repositories/account.repository.ts
@@ -1,9 +1,10 @@
 import { model, Model } from 'mongoose'
 import { ShipperInterface } from '../entities/interfaces/data/shipper.interface'
 import { ShipperSchema } from '../entities/schemas/shipper.schema'
-import { createDTO, identifierDTO ,updateProfileDTO, whitelistUpdateProfileDTO } from '../entities/dtos/shipper.dto'
+import { createDTO, identifierDTO, updateProfileDTO, whitelistUpdateProfileDTO } from '../entities/dtos/shipper.dto'
 
 import config from '../config/config'
+import { JobInterface } from 'src/entities/interfaces/data/job.interface'
 
 class AccountRepository {
   private static instance: AccountRepository
@@ -22,19 +23,24 @@ class AccountRepository {
     return AccountRepository.instance
   }
 
-  public async adminFindShipperByIdentifier(identifier: identifierDTO): Promise<ShipperInterface | null> {
+  public async srvFindShipperByIdentifier(identifier: identifierDTO): Promise<ShipperInterface | null> {
     const result: ShipperInterface | null = await this._model.findOne(identifier)
-    return result 
+    return result
   }
 
   public async findShipperByIdentifier(identifier: identifierDTO): Promise<ShipperInterface | null> {
-    const result: ShipperInterface | null = await this._model.findOne(identifier, { _id: 0, password: 0, created_at: 0, updated_at: 0})
-    return result 
+    const result: ShipperInterface | null = await this._model.findOne(identifier, {
+      _id: 0,
+      password: 0,
+      created_at: 0,
+      updated_at: 0,
+    })
+    return result
   }
 
   public async findPasswordHashedByIdentifier(identifier: identifierDTO): Promise<string | null> {
-    const { password : hashed }  = await this._model.findOne(identifier) as ShipperInterface
-    return hashed 
+    const { password: hashed } = (await this._model.findOne(identifier)) as ShipperInterface
+    return hashed
   }
 
   public async createShipperAccount(shipper_account: createDTO): Promise<string> {
@@ -42,13 +48,16 @@ class AccountRepository {
     const { shipper_id } = await mongooseModel.save()
     return shipper_id as string
   }
-  
+
   public async updateEmailByIdentifier(identifier: identifierDTO, email: string): Promise<string> {
-    const { shipper_id } = await this._model.updateOne(identifier, { $set: { email }})
+    const { shipper_id } = await this._model.updateOne(identifier, { $set: { email } })
     return shipper_id as string
   }
 
-  public async updateProfileShipperAccountByIdentifier(identifier: identifierDTO, profile: whitelistUpdateProfileDTO): Promise<string> {
+  public async updateProfileShipperAccountByIdentifier(
+    identifier: identifierDTO,
+    profile: whitelistUpdateProfileDTO,
+  ): Promise<string> {
     const { shipper_id } = await this._model.updateOne(identifier, { $set: profile })
     return shipper_id as string
   }
@@ -59,12 +68,12 @@ class AccountRepository {
   }
 
   public async deActivateShipperAccount(identifier: identifierDTO, username: string, bias: string): Promise<number> {
-    const result = await this._model.updateOne(identifier, { $set: { username : bias + username }})
+    const result = await this._model.updateOne(identifier, { $set: { username: bias + username } })
     return result.nModified as number
   }
 
-  public async updateJobHistory(shipper_id: string, job_id: string): Promise<number> {
-    const result = await this._model.updateOne({ shipper_id }, { $push: { "jobs" : job_id as any } })
+  public async updateJobHistory(identifier: identifierDTO, job: JobInterface): Promise<number> {
+    const result = await this._model.updateOne(identifier, { $push: { job_history: job as JobInterface } })
     return result.n
   }
 }
