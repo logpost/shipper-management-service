396999497a722c39e8d0477d11a2f4077cf7d12a
poonsht <poon_arsene_lupin@hotmail.com>
2021-01-24 19:55:26 +0700

    [+] add : enable Profiler

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import * as Profiler from '@google-cloud/profiler'
 import App from './app'
 
 import AuthPlugin from './plugins/auth.plugin'
@@ -5,6 +6,16 @@ import CorsPlugin from './plugins/cors.plugin'
 
 import ShipperRoute from './routes/shipper.route'
 import JobRote from './routes/job.route'
+
+if (process.env.NODE_ENV === 'staging') {
+  Profiler.start({
+    projectId: 'logpost-298506',
+    serviceContext: {
+      service: 'shipper-management-service',
+      version: '1.0.0',
+    },
+  })
+}
 
 const app = new App({
   routes: [ShipperRoute, JobRote],
